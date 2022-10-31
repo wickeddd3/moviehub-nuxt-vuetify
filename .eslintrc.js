69704cce2bb2cfb06ab4715890d0f22bd02af04d
wickeddd3 <pablophilipandrew@gmail.com>
2022-11-01 00:15:18 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:wdio/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [
+    'wdio'
+  ],
+  // add your custom rules here
+  rules: {}
+}
