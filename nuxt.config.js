82f29c99e0ea696faef4046789bdf2ee5030df4a
wickeddd3 <pablophilipandrew@gmail.com>
2023-01-18 02:10:15 +0800

    generate docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     standalone: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/docs/moviehub-nuxt-vuetify/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/moviehub-nuxt-vuetify/docs/' : '/',
   },
 
   generate: {
